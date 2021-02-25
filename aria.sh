2c2adf2abe542c5fbbbe4d00ef16c849056861a8
yamraaj007 <69627019+crazy-racer@users.noreply.github.com>
2021-02-26 01:14:32 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,7 +1,10 @@
 export MAX_DOWNLOAD_SPEED=0
-export MAX_CONCURRENT_DOWNLOADS=3
+tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/best.txt | awk '$1' | tr '\n' ',')
+export MAX_CONCURRENT_DOWNLOADS=25
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
   --max-connection-per-server=10 --rpc-max-request-size=1024M \
-  --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
-   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
-   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+  --bt-tracker="[$tracker_list]" --bt-max-peers=0 --seed-time=30 \
+  --min-split-size=10M --follow-torrent=mem --split=10 \
+   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED --bt-stop-timeout=480 \
+   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
+   --peer-agent=qBittorrent/4.2.5 --peer-id-prefix=-qB4250-
